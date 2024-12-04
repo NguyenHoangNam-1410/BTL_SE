f164bf47639f2edd8f400cc801bc339e12fd2241
NguyenHoangNam-1410 <nam.nguyensilv666@hcmut.edu.vn>
2024-12-04 09:02:44 +0700

    Delete BTL_SE-main/backend directory

--- BTL_SE-main/backend/models/PermittedFileType.js
+++
@@ -1,8 +0,0 @@
-class PermittedFileType {
-    constructor({ fileTypeId, mimeType }) {
-        this.fileTypeId = fileTypeId;
-        this.mimeType = mimeType;
-    }
-  }
-  
-  export default PermittedFileType;
